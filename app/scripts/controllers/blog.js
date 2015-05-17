fc6050b420da0994eb5b4a8d234a4d609e203ac2
mohsom <volodya.skalskyi@gmail.com>
2015-05-17 19:33:33 +0300

    Commit.

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -8,9 +8,16 @@
  * Controller of the personalApp
  */
 angular.module('personalApp')
-  .controller('BlogCtrl', function ($scope) {
+  .controller('BlogCtrl', function ($scope,$http) {
+    $scope.posts=[];
     $scope.init = function(){
       $('title').html('Blog');
+      $scope.getPosts();
+    };
+    $scope.getPosts=function(){
+      $http.get('json/posts.json').success(function(data){
+        $scope.posts=data;
+      });
     };
     $scope.init();
   });
